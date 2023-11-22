5edd51116539c2df0df50be91fd1713777280657
x417v <ilyakinskiy@gmail.com>
2023-11-22 22:23:21 +0200

    Implement Login and Logout

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { databaseConfig } from './config/configuration';
 import { ConfigModule } from '@nestjs/config';
 import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { SequelizeConfigService } from './config/sequelizeConfig.service';
       load: [databaseConfig],
     }),
     UsersModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,17 +5,43 @@ import {
   HttpCode,
   Header,
   Body,
+  Request,
+  UseGuards,
+  Get,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from 'src/auth/local.auth.guard';
+import { AuthenticatedGuard } from 'src/auth/authenticated.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
+
   @Post('/signup')
   @HttpCode(HttpStatus.CREATED)
   @Header('Content-type', 'application/json')
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Header('Content-type', 'application/json')
+  login(@Request() req) {
+    return { user: req.user, msg: 'Logged in' };
+  }
+
+  @Get('/login-check')
+  @UseGuards(AuthenticatedGuard)
+  loginCheck(@Request() req) {
+    return req.user;
+  }
+
+  @Get('/logout')
+  logout(@Request() req) {
+    req.session.destroy();
+    return { msg: 'Session has ended' };
   }
 }
